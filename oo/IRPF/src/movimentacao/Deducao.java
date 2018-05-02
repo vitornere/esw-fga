23b233e6c563c312244d25666b1d6c5f665ae283
Andre Luiz Peron Martins Lanna <andrelanna@MacBook-Air-de-Andre-3.local>
2018-05-02 15:34:18 -0300

    [oo] pacote movimentacao implementado

---
+++ oo/IRPF/src/movimentacao/Deducao.java
@@ -0,0 +1,14 @@
+package movimentacao;
+
+public class Deducao {
+	String descricao;
+	float valor;
+	
+	Deducao() {
+	}
+	
+	Deducao (String d, float v) {
+		descricao = d;
+		valor = v;
+	}
+}
---
+++ oo/IRPF/src/movimentacao/Rendimento.java
@@ -0,0 +1,14 @@
+package movimentacao;
+
+public class Rendimento {
+	String descricao;
+	float valor;
+	
+	Rendimento() {
+	}
+	
+	Rendimento(String d, float v) {
+		descricao = d;
+		valor = v;
+	}
+}
